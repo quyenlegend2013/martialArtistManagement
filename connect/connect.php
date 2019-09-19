c21d1a0003d7d0199be0b8fafba15bc1b260eb10
Vuong Di Quyen <55535242+quyenlegend2013@users.noreply.github.com>
2019-09-19 23:01:50 +0700

    update

--- connect/connect.php
+++ connect/connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "martialmanagement";
+$servername = "den1.mssql8.gear.host";
+$username = "martial";
+$password = "martial@";
+$dbname = "martial";
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 ?>
